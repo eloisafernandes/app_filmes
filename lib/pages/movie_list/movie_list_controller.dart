ba5b2f3e1f1df105435b28ccbbd9de0e75a0e96a
eloisafernandes <eloisafer16@gmail.com>
2024-01-27 23:55:44 +0000

    rolagem infinita nos filmes

--- lib/pages/movie_list/movie_list_controller.dart
+++ lib/pages/movie_list/movie_list_controller.dart
@@ -3,21 +3,47 @@ import 'dart:async';
 import 'package:app_filmes/data/models/movie.dart';
 import 'package:app_filmes/data/movie_api.dart';
 import 'package:app_filmes/service_locator.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class MovieListController{
   final api = getIt<MovieApi>();
 
   final _controller =  StreamController<List<Movie>>();
-
   Stream<List<Movie>> get stream => _controller.stream;
-
+  final int _pageSize = 20;
+  final PagingController<int, Movie> pagingController = PagingController(firstPageKey: 0);
 
   void init(){
-    getMovies();
+    pagingController.addPageRequestListener((pageKey) => getMovies(pageKey));
+    //getMovies();
   }
 
-  Future<void> getMovies() async{
-    var result = await api.getMovies();
-    _controller.sink.add(result);
+  
+
+  Future<void> getMovies(int pageKey) async{
+    try{
+      var newItems = await api.getMovies(skip: pageKey, take: _pageSize);
+
+
+      final isLastPage = newItems.length < _pageSize;
+
+      if (isLastPage){
+        pagingController.appendLastPage(newItems);
+      }
+      else{
+        final nextPageKey = pageKey +newItems.length;
+        pagingController.appendPage(newItems, nextPageKey);
+
+      }
+    } catch (error) {
+      pagingController.error = error;
+    }
   }
+
+  // Future<void> getMovies() async{
+  //   var result = await api.getMovies();
+  //   _controller.sink.add(result);
+  // }
+
+
 }
--- lib/pages/movie_list/movie_list_page.dart
+++ lib/pages/movie_list/movie_list_page.dart
@@ -5,6 +5,7 @@ import 'package:app_filmes/pages/movie_list/widgets/movie_item_widget.dart';
 import 'package:app_filmes/service_locator.dart';
 import 'package:app_filmes/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class MovieListPage extends StatefulWidget {
   const MovieListPage({super.key});
@@ -18,7 +19,7 @@ class _MovieListPage extends State<MovieListPage> {
 
   @override
   void initState() {
-    
+
     controller.init();
     super.initState();
   }
@@ -37,23 +38,31 @@ class _MovieListPage extends State<MovieListPage> {
         ],
 
       ),
-      body: StreamBuilder<List<Movie>>(
-        stream: controller.stream,
-        builder:(context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting){
-            return const ProgressIndicatorWidget();
-          }
+      body: PagedListView<int, Movie>(
+        pagingController: controller.pagingController,
+        builderDelegate: PagedChildBuilderDelegate(
+          itemBuilder: (context, movie, index) => MovieItemWidget(movie: movie),
+          firstPageProgressIndicatorBuilder:(_) => const ProgressIndicatorWidget(),
+          newPageProgressIndicatorBuilder: (_) => const ProgressIndicatorWidget(),
+        )
+      ),
+      // body: StreamBuilder<List<Movie>>(
+      //   stream: controller.stream,
+      //   builder:(context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting){
+      //       return const ProgressIndicatorWidget();
+      //     }
 
-          var movies = snapshot.data!;
-          return ListView.builder(
-            itemCount: movies.length,
-            itemBuilder: (context, index) {
-              var movie = movies[index];
-              return MovieItemWidget(movie: movie);
-            },
-          );
-        },
-      )
+      //     var movies = snapshot.data!;
+      //     return ListView.builder(
+      //       itemCount: movies.length,
+      //       itemBuilder: (context, index) {
+      //         var movie = movies[index];
+      //         return MovieItemWidget(movie: movie);
+      //       },
+      //     );
+      //   },
+      // )
 
     );
   }
